e3e721ab137d09ff370285b75535f4fd871d3def
Søren Louv-Jansen <sl@konscript.com>
2011-09-02 02:01:56 +0200

    Forgot to include new files

---
+++ wp-content/themes/konscript/no-sidebar.php
@@ -0,0 +1,9 @@
+<?php /* Template Name: No Sidebar*/ ?>
+
+<?php get_header(); ?>
+
+		<div id="content" role="main">
+                <?php echo the_content(); ?>
+		</div>
+
+<?php get_footer(); ?>
