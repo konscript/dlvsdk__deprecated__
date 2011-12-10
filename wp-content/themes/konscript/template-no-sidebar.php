272ede565a8fa39c084be5868c7958f727864384
Søren Louv-Jansen <sl@konscript.com>
2011-12-10 23:31:01 +0100

    Added "No sidebar" template

---
+++ wp-content/themes/konscript/template-no-sidebar.php
@@ -0,0 +1,18 @@
+<?php /* Template Name: No sidebar*/ ?>
+<?php get_header(); ?>
+<?php the_submenu(); ?>
+
+<div id="content">	
+    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+
+        <div class="post">
+            <h1><?php the_title(); ?></h1>
+            <div class="post-content">
+                <?php echo the_content(); ?>
+            </div>
+	    </div><!--#end post-->
+	    		    
+    <?php endwhile; endif; ?>
+</div><!--#end content -->
+
+<?php get_footer(); ?>
