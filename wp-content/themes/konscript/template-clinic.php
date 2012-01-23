f97cc8be1ef83fc6dc61c21e612f096186af6a88
laander <la@laander.com>
2012-01-23 04:46:10 +0100

    more horsesh

--- wp-content/themes/konscript/template-clinic.php
+++ wp-content/themes/konscript/template-clinic.php
@@ -1,6 +1,16 @@
 <?php /* Template Name: Clinic*/ ?>
 <?php get_header(); ?>
-<?php include("menus/clinic.php"); ?>
+<?php
+// Sidebar menu
+$args = array(
+  'post_type'=>'clinic',
+  'title_li'=> '&nbsp;',
+  'echo' => false,  
+);
+$sidebar_menu = wp_list_pages( $args );
+$clinic = basename(get_permalink());
+$sidebar_book = '<a class="button-book" href="' . get_bloginfo('wpurl') . '/booking/clinic/' . $clinic . '"><div class="button-book-title">Book your vaccination</div></a>';
+the_submenu($sidebar_menu . $sidebar_book); ?>
 
 <section id="primary">
 	<div id="content" role="main">
