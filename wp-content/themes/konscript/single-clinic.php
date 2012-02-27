eeccd63681ee44db541557ba6078b0e0c8c880bc
Søren Louv-Jansen <sl@konscript.com>
2012-02-27 15:42:37 +0100

    Changed opening hours ACF

--- wp-content/themes/konscript/single-clinic.php
+++ wp-content/themes/konscript/single-clinic.php
@@ -31,13 +31,19 @@ sidebar($sidebar_menu, false, false); ?>
 						<p class="address">Address: <?php the_field('address'); ?>, <?php the_field('city'); ?></p>					
 						<p class="telephone">Phone: <?php the_field('phone_number'); ?></p>
 					</div>	
-				
+					
+					<?php $weekdays = array("monday", "tuesday", "wednesday", "thursday", "friday"); ?>						
 					<div class="opening_hours">	
 						<p class="header">Opening hours</p>
 						<table class="zebra">
-							<?php while(the_repeater_field('opening_hours')): ?>					
-								<tr><td><?php the_sub_field('day_of_week'); ?></td><td><?php the_sub_field('hours'); ?></td></tr>									
-							<?php endwhile; ?>
+							<?php	foreach($weekdays as $weekday): ?>
+									
+								<?php 
+								$hours = get_field($weekday);
+								if($hours != ""): ?>
+									<tr><td><?php echo ucfirst($weekday); ?></td><td><?php echo $hours; ?></td></tr>									
+								<?php	endif; ?>								
+							<?php	endforeach; ?>
 						</table>			
 					</div>
 					
